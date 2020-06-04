8a6ac3d02d72376fd8ccc4e917d843002b598bfe
Harun Tekin <haruntekin@Haruns-MacBook-Air.local>
2020-06-04 21:47:20 +0200

    trd

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 
+app.use('/', (req, res, next) => {
+    console.log(req.ip);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Yavsak Cuneyt');
 });
